80b67f063b25b98347352530bac60d89d855d338
Maciej <maprz2017@gmail.com>
2025-09-16 18:10:31 +0200

    Added hints and hint manager

---
+++ RGBall/Assets/Scripts/HintManager/HintManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class HintManager : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInputs();
+    }
+
+    void HandleInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            GameObject child;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                child = transform.GetChild(i).gameObject;
+                child.SetActive(!child.activeInHierarchy);
+            }
+        }
+    }
+}
